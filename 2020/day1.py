96f23fdbc022a828dc5e7b0052429023f4f1286f
Marcelo <mmhcastro@gmail.com>
2020-12-01 08:30:03 +0100

    Code formatting

--- 2020/day1.py
+++ 2020/day1.py
@@ -1,26 +1,27 @@
 def parse_program(filepath):
     with open(filepath, "r") as f:
-        program = [x for x in map(int, f.read().split('\n'))]
+        program = [x for x in map(int, f.readlines())]
     return program
 
-def find_sum(expenses, total):
 
+def find_sum(expenses, total):
     for i in range(len(expenses)):
         for j in range(i, len(expenses)):
-            if(expenses[i]+expenses[j] == total):
-                print(expenses[i]*expenses[j])
+            if expenses[i] + expenses[j] == total:
+                print(expenses[i] * expenses[j])
+
 
 def find_triple_sum(expenses, total):
-
     expenses.sort()
-    
+
     for i in range(len(expenses)):
         for j in range(i, len(expenses)):
             for k in range(j, len(expenses)):
-                if(expenses[i]+expenses[j]+expenses[k] == total):
-                    print(expenses[i]*expenses[j]*expenses[k])
-                if(expenses[i]+expenses[j]+expenses[k] > total):
+                if expenses[i] + expenses[j] + expenses[k] == total:
+                    print(expenses[i] * expenses[j] * expenses[k])
+                if expenses[i] + expenses[j] + expenses[k] > total:
                     break
+
 
 if __name__ == "__main__":
     data = parse_program("input/day1.data")
