71302372e1a9d3895aa10f02433f0bb018e20a0e
Marcelo <mmhcastro@gmail.com>
2020-12-02 07:56:53 +0100

    2020 day 2

---
+++ 2020/day2.py
@@ -0,0 +1,33 @@
+import re
+
+
+def parse_input(filepath):
+    with open(filepath, "r") as f:
+        passwords = []
+        for line in f.readlines():
+            parsed = re.search('(\d+)-(\d+) (.): (.+)', line).groups()
+            passwords.append(parsed)
+    return passwords
+
+
+def count_valid(passwords):
+    valid = 0
+    for low, high, letter, password in passwords:
+        if int(low) <= password.count(letter) <= int(high):
+            valid += 1
+    print(valid)
+
+
+def match_valid(passwords):
+    valid = 0
+    for low, high, letter, password in passwords:
+        matches = int(password[int(low) - 1] == letter) + int(password[int(high) - 1] == letter)
+        if matches == 1:
+            valid += 1
+    print(valid)
+
+
+if __name__ == "__main__":
+    passwords = parse_input("input/day2.data")
+    count_valid(passwords)
+    match_valid(passwords)
